7a1cb83bd5316295ff098a2ec64a97aa965197bb
Michael <Michael.Rommel2@de.bosch.com>
2023-01-29 17:00:01 +0100

    add state test

--- tests/test_animation.py
+++ tests/test_animation.py
@@ -1,6 +1,6 @@
 import pytest
 
-from animations.models import AnimationModel
+from animations.models import AnimationModel, ObjectModel, ObjectType, ObjectAnimationModel, AnimationType
 
 
 @pytest.mark.django_db
@@ -19,3 +19,44 @@ def test_animation_creation():
 	assert animation.height == 100
 	assert animation.start == 0
 	assert animation.end == 10
+
+
+@pytest.mark.django_db
+def test_animation_model():
+	new_animation = AnimationModel()
+	new_animation.name = 'dummy'
+	new_animation.width = 100
+	new_animation.height = 100
+	new_animation.start = 0
+	new_animation.end = 10
+	new_animation.save()
+
+	obj = ObjectModel()
+	obj.name = 'base object'
+	obj.animation = new_animation
+	obj.type = ObjectType.UNKNOWN
+	obj.zindex = 1
+	obj.image = None
+	obj.x_coord = 10
+	obj.y_coord = 10
+	obj.width = 80
+	obj.height = 50
+	obj.color = '#FFFFFF'
+	obj.save()
+
+	ani = ObjectAnimationModel()
+	ani.object = obj
+	ani.type = AnimationType.MOVE
+	ani.start = 2
+	ani.end = 5
+	ani.dx = 20
+	ani.dy = 0
+	ani.scale = 1.0
+	ani.save()
+
+	state = obj.stateIn(0, 7)
+
+	assert state.x == 36.66666666666667
+	assert state.y == 10
+	assert state.width == 80
+	assert state.height == 50
--- tests/tests.py
+++ tests/tests.py
@@ -1,7 +1,7 @@
 """ unittest module """
 import unittest
 
-from animations.models import AnimationModel
+from animations.models import AnimationModel, ObjectType, ObjectModel, AnimationType, ObjectAnimationModel
 
 
 class TestBasic(unittest.TestCase):
@@ -9,15 +9,3 @@ class TestBasic(unittest.TestCase):
 		"""Test the  constructor"""
 
 		self.assertEqual(2 + 2, 4)
-
-
-class TestBasic2(unittest.TestCase):
-	def test_constructor(self):
-		"""Test the  constructor"""
-
-		animation = AnimationModel()
-		animation.name = 'dummy'
-		animation.width = 100
-		animation.height = 100
-		animation.start = 0
-		animation.end = 10
